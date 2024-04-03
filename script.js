3ea8b7cb0e16ff3ee58b88158a62ce9346c0c9fa
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-04-03 20:44:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here!
+function addListItem() {
+    let li = document.createElement("li");
+    li.textContent = 'Item ' + (document.querySelectorAll('#infi-list li').length + 1);
+    document.getElementById("infi-list").appendChild(li);
+}
 
+for(let i=0; i<10; i++) {
+    addListItem();
+}
+window.addEventListener('scroll', function() {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        // User has reached the end of the page, add two more items
+        addListItem();
+        addListItem();
+    }
+});
